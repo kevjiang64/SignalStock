80b30e8ef4dc4373b4708cc21a63d18877e6ab0f
welleh <kevinjiang64@hotmail.com>
2025-10-18 02:15:04 -0400

    Implement search stocks

--- components/NavItems.tsx
+++ components/NavItems.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import {usePathname} from "next/navigation";
 import {NAV_ITEMS} from "@/lib/constants";
 import Link from "next/link";
+import SearchCommand from "@/components/SearchCommand";
 
 export const NavItems = () => {
 
@@ -17,14 +18,25 @@ export const NavItems = () => {
 
     return (
         <ul className="flex flex-col sm:flex-row p-2 gap-3 sm:gap-10 font-medium">
-            {NAV_ITEMS.map(({href, label}) => (
-                <li key={href}>
+            {NAV_ITEMS.map(({href, label}) => {
+
+                if(label === "Search") return (
+                    <li key={"search-trigger"}>
+                        <SearchCommand
+                        renderAs={"text"}
+                        label={"Search"}
+                        initialStocks={[]}
+                        />
+                    </li>
+                )
+
+                return <li key={href}>
                     <Link href={href} className={`hover:text-yellow-500 transition-colors 
                     ${isActive(href) ? 'text-gray-100' : ``}`}>
                         {label}
                     </Link>
                 </li>
-            ))}
+            })}
         </ul>
     )
 }
